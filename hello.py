778d91534b088f06ef2005f764bcaaf7797b614f
Henrique Duran <henriqueduran15@gmail.com>
2023-09-11 11:32:24 -0300

    :sparkles: simple students CRUD

--- hello.py
+++ hello.py
@@ -1,32 +1,25 @@
 import asyncio
-
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask import request
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-uri = "mongodb://127.0.0.1:27017"
+mongo_password = os.getenv("MONGO_PASSWORD")
+uri = f"mongodb+srv://uspolis:{mongo_password}@cluster0.melr7t2.mongodb.net/?retryWrites=true&w=majority"
 
-client = MongoClient('localhost', 27017)
+client = MongoClient(uri, 27017)
 
 db = client.flask_db
 things = db.things
+students_collection = db.students
 
 
 @app.get("/")
 def hello_world():
     return {"hello": "world"}
-
-
-@app.get("/ping")
-def ping():
-    try:
-        print("Pinged")
-        return "<p> Pinged </p>"
-    except Exception as e:
-        print(e)
-        return "<p> Not Pinged </p>"
 
 
 @app.post("/something")
@@ -35,3 +28,51 @@ def post_something():
     something = data["something"]
     things.insert_one({"thing": something})
     return f"Inserted {something}"
+
+
+@app.post('/students')
+def create_student():
+    student_data = request.json
+    if student_data:
+        result = students_collection.insert_one(student_data)
+        return {"message": "Student created successfully", "student_id": str(result.inserted_id)}, 201
+    else:
+        return {"error": "Invalid data"}, 400
+
+
+@app.get('/students')
+def get_students():
+    students = list(students_collection.find({}))
+    return {"students": students}
+
+
+@app.get('/students/<string:student_id>')
+def get_student(student_id):
+    student = students_collection.find_one({"_id": student_id})
+    if student:
+        return student
+    else:
+        return {"error": "Student not found"}, 404
+
+
+@app.put('/students/<string:student_id>')
+def update_student(student_id):
+    student_data = request.json
+    if student_data:
+        result = students_collection.update_one(
+            {"_id": student_id}, {"$set": student_data})
+        if result.modified_count > 0:
+            return {"message": "Student updated successfully"}
+        else:
+            return {"error": "Student not found"}, 404
+    else:
+        return {"error": "Invalid data"}, 400
+
+
+@app.delete('/students/<string:student_id>')
+def delete_student(student_id):
+    result = students_collection.delete_one({"_id": student_id})
+    if result.deleted_count > 0:
+        return {"message": "Student deleted successfully"}
+    else:
+        return {"error": "Student not found"}, 404
